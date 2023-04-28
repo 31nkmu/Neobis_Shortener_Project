c4ec2585cf3dae790198e56e3cbdad93e598a0b9
billal <karimovbillal20002@gmail.com>
2023-04-29 01:16:01 +0600

    added views

--- config/url_app/models.py
+++ config/url_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Url(models.Model):
+    url = models.URLField(unique=True)
+    short_url = models.CharField(max_length=20, blank=True, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-created_at',)
+
--- config/url_app/views.py
+++ config/url_app/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework.mixins import CreateModelMixin, DestroyModelMixin, ListModelMixin
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
 
-# Create your views here.
+from url_app.models import Url
+from url_app.serializers import UrlSerializer, GetFullLinkSerializer
+
+
+class UrlViewSet(CreateModelMixin,
+                 DestroyModelMixin,
+                 ListModelMixin,
+                 GenericViewSet):
+    queryset = Url.objects.all()
+    serializer_class = UrlSerializer
+
+
+class GetFullLinkAPIView(APIView):
+    def post(self, request):
+        serializer = GetFullLinkSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        short_url = request.data['short_url']
+        url = Url.objects.filter(short_url=short_url)
+        if url.exists():
+            return Response({'full_url': url[0].url}, status=status.HTTP_200_OK)
+        return Response({'msg': 'нет такой ссылки'}, status=status.HTTP_400_BAD_REQUEST)
